2c7165f899858cacab6243ffd19ffc6b46cc55c3
ssddf9/ssddf111 <56322415+ssddf9@users.noreply.github.com>
2020-05-01 00:38:44 -0600

    Update Ready_screen.py

--- Ready_screen.py
+++ Ready_screen.py
@@ -1,10 +1,14 @@
 def ready(lvl, screen):
-    import pygame
+    import pygame, sys
     screen.fill((0, 0, 255))
     pygame.font.init()
     fontToUse = pygame.font.SysFont('Courier New Bold.ttf', 286)
     text = fontToUse.render('Ready?', False, (0, 255, 0))
     screen.blit(text, (screen.get_size()[0] / 6, screen.get_size()[1] / 6))
+    for i in range(0, screen.get_size()[0], 4):
+        pygame.draw.line(screen, (255, 255, 0), (i, 0), (i, screen.get_size()[1]))
+    for i in range(0, screen.get_size()[1], 4):
+        pygame.draw.line(screen, (255, 255, 0), (0, i), (screen.get_size()[0], i))
     while True:
         for event in pygame.event.get():
             if event.type == pygame.KEYDOWN:
