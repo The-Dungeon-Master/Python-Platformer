2a05283c481d71ae340cb0cad634c876bb5e95b3
ssddf9/ssddf111 <56322415+ssddf9@users.noreply.github.com>
2020-05-02 12:30:05 -0600

    Update platformer.py

--- platformer.py
+++ platformer.py
@@ -1,4 +1,4 @@
-import platformer_lvl1, platformer_lvl2, platformer_lvl3, platformer_lvl4, pygame
+import platformer_lvl1, platformer_lvl2, platformer_lvl3, platformer_lvl4, platformer_lvl5, pygame
 timeTaken = platformer_lvl1.levelOne()
 timeTaken = platformer_lvl2.levelTwo(timeTaken)
 levelPassed = False
@@ -7,4 +7,7 @@ while not levelPassed:
 levelPassed = False
 while not levelPassed:
     timeTaken, levelPassed = platformer_lvl4.levelFour(timeTaken)
+levelPassed = False
+while not levelPassed:
+    timeTaken, levelPassed = platformer_lvl5.levelFive(timeTaken)
 pygame.quit()
